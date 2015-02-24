564acda56e9c3ad11368616ac9bf05b466ab788b
Pooji <pooji146@gmail.com>
2015-02-24 15:02:09 +0530

    first blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :blogs
+
+  get "pooji" => "home#home"
+
+  get "about" => "home#about"
+
+  get "contactus" =>"home#contactus"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
